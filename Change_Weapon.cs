8904df563a859198cb2c35b24fd9cfeb2c330db6
BungPeerapat <mashiro74122541@gmail.com>
2020-03-27 19:22:09 +0700

    Save

--- Change_Weapon.cs
+++ Change_Weapon.cs
@@ -8,24 +8,15 @@ public class Change_Weapon : MonoBehaviour
     public GameObject Handgun_01_FPSController;
     void Update()
     {
-        int cw = 0;
         if (Input.GetKeyDown("c"))
-        {
-            cw += 1;
-        }
-        if (cw == 1)
         {
             Assault_Rifle_01_FPSController.SetActive(true);
             Handgun_01_FPSController.SetActive(false);
         }
-        else if (cw == 2)
+        if (Input.GetKeyDown("v"))
         {
             Assault_Rifle_01_FPSController.SetActive(false);
             Handgun_01_FPSController.SetActive(true);
-        }
-        else
-        {
-            cw = 1;
         }
     }
 }
