ec28a574da986f367d7562d5e39d33f2f562406d
BungPeerapat <mashiro74122541@gmail.com>
2020-03-27 17:29:42 +0700

    save

--- Script/PlayerMovement.cs
+++ Script/PlayerMovement.cs
@@ -12,10 +12,23 @@ public class PlayerMovement : MonoBehaviour
     int floodMask;
     float camRayLenght = 100f;
 
+    //=========Mouse=====================
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    float rotationY = 0F;
+
+   //============Mouse==================
     void Start()
     {
         PlayerRGB = GetComponent<Rigidbody>();
         Animator = GetComponent<Animator>();
+        PlayerRGB.freezeRotation = true;
     }
 
     void FixedUpdate()
@@ -23,6 +36,24 @@ public class PlayerMovement : MonoBehaviour
         float h = Input.GetAxis("Horizontal"); //แนวนอน ซ้าย ขวา
         float v = Input.GetAxis("Vertical"); //แนวตั้ง ชึ้น ลง
         Move(h,v);
+        float rh = Input.GetAxis("Mouse X");
+        float rv = Input.GetAxis("Mouse Y");
+        //================Mouse======
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        //==========Mouse==========
+
     }
     void Move (float h, float v)
     {
