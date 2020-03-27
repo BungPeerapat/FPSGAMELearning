1e8bfa4af3a24b9df1239deb908e948809618282
BungPeerapat <mashiro74122541@gmail.com>
2020-03-27 18:35:32 +0700

    Save

--- Change_Weapon.cs
+++ Change_Weapon.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Change_Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject Assault_Rifle_01_FPSController;
+    public GameObject Handgun_01_FPSController;
+    public bool ChangeWeapon;
     void Update()
     {
-        
+        if (ChangeWeapon == true)
+        {
+            Assault_Rifle_01_FPSController.SetActive(true);
+            Handgun_01_FPSController.SetActive(false);
+        }
+        else
+        {
+            Handgun_01_FPSController.SetActive(true);
+            Assault_Rifle_01_FPSController.SetActive(false);
+        }
     }
 }
