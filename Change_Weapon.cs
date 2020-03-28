d193544431ca6cdc2c12d13080855c8a25de3d8b
BungPeerapat <mashiro74122541@gmail.com>
2020-03-28 15:41:05 +0700

    Save

--- Change_Weapon.cs
+++ Change_Weapon.cs
@@ -8,8 +8,6 @@ public class Change_Weapon : MonoBehaviour
     public GameObject Handgun_01_FPSController;
     public float TAKH = Input.GetAxis("Horizontal");
     public float TAKV = Input.GetAxis("Vertical");
-    public float TPTH = Input.GetAxis("Horizontal");
-    public float TPTV = Input.GetAxis("Vertical");
     void FixedUpdate()
     {
         float TAKV = Input.GetAxis("Vertical");
@@ -17,20 +15,19 @@ public class Change_Weapon : MonoBehaviour
         float TPTV = Input.GetAxis("Vertical");
         if (Input.GetAxis("Mouse ScrollWheel") > 0f)
             {
+                Tranpositionweapon();
                 Assault_Rifle_01_FPSController.SetActive(true);
                 Handgun_01_FPSController.SetActive(false);
                 Tranpositionweapon();
             }
             if (Input.GetAxis("Mouse ScrollWheel") < 0f)
             {
+                Tranpositionweapon();
                 Assault_Rifle_01_FPSController.SetActive(false);
                 Handgun_01_FPSController.SetActive(true);
-                Tranpositionweapon();
             }
     }
     void Tranpositionweapon()
     {
-        gameObject.transform.Translate(TAKH, 0f, TAKV);
-        gameObject.transform.Translate(TPTH, 0f, TPTV);
-    }
+        gameObject.transform.Translate(TAKH, 0f, TAKV);    }
 };
