cfca70819634d11da19b182765a12c33240d2196
BungPeerapat <mashiro74122541@gmail.com>
2020-03-28 18:45:15 +0700

    Save

---
+++ Script/CountDownforStartgame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownforStartgame : MonoBehaviour
+{
+    public int CountDownnumber;
+    public Text CountDownShowText;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        while(CountDownnumber >= 3)
+        {
+            CountDownShowText.text = CountDownnumber.ToString();
+            CountDownnumber += 1;
+        }
+
+        CountDownShowText.text = "Let Start!";
+    }
+}
